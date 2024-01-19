eef6909e125a5c486b1d56d1f3dcd48ba830d9f9
Gerardo Heredia <gerardo.heredia@protonmail.com>
2024-01-19 13:33:06 -0600

    Initial commit & first page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageOneComponent } from './pages/page-one/page-one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page-one', component: PageOneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageOneComponent } from './pages/page-one/page-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageOneComponent
   ],
   imports: [
     BrowserModule,
